b356fb6dc8d9d13d6f7b0824294abe11fb1f312e
mohamed <msam13716@gmail.com>
2023-09-07 11:18:35 -0700

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entery point 
+ *
+ * discription: a c programming print put function
+ *
+ * return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+	retur (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * description: a c program print the printf funtion
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
