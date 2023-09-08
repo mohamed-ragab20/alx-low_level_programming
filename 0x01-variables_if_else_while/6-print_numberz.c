cfc89cb1d47f760a419859a8ad67b71142b854b5
mohamed <msam13716@gmail.com>
2023-09-08 10:31:58 -0700

    My commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: print 0-9 using putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n')'
+
+	return (0);
+}
