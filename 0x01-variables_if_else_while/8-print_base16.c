7d4a3347857c9e66edcd4c23c73723be12a9bcb4
mohamed <msam13716@gmail.com>
2023-09-08 10:50:13 -0700

    My commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <sdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: print number of base16 in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
