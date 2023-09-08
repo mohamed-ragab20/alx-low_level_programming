b991c8233e1e2b267b51b4448e33fed8d7789411
mohamed <msam13716@gmail.com>
2023-09-08 09:03:45 -0700

    My commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,15 +5,18 @@
 /**
  * main _ Entry point
  *
- * Description: true or false validation to know positive or negative
+ * Description: true or false validation
  *
  * Return: 0 (success)
 */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%i is positive \n". n);
 	else if (n == 0)
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ *
+ * Description: print alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
