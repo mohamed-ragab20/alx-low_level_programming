29a93eeb18ecf6d2aca91b65d0f93169f67f9501
mohamed <msam13716@gmail.com>
2023-09-12 12:32:50 -0700

    My commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ void jack_bauer(void)
 	{
 		for (ment = 0; ment <= 59; ment++)
 		{
-			_putchart((hor / 10) + 48);
-			_putchart((hor % 10) + 48);
-			_putchart(':');
-			_putchart((ment / 10) + 48);
-			_putchart((ment % 10) + 48);
-			_putchart('\n');
+			_putchar((hor / 10) + 48);
+			_putchar((hor % 10) + 48);
+			_putchar(':');
+			_putchar((ment / 10) + 48);
+			_putchar((ment % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
