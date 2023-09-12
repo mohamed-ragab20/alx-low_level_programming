2e94434cf9f46f8c3dae6686fe3ca3d6728e183d
mohamed <msam13716@gmail.com>
2023-09-12 13:58:15 -0700

    My commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time table
+ *
+ * @n: takes num input
+*/
+
+void print_times_table(int n)
+{
+	int pro, mu, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar(48);
+		for (mu = 1; mu <= n; mu++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			pro = num * mu;
+
+			if (pro <= 9)
+				_putchar(' ');
+			if (pro <= 99)
+				_putchar(' ');
+			if (pro >= 100)
+			{
+				_putchar((pro / 100) + 48);
+				_putchar((pro / 10) % 10 + 48);
+			}
+			else if (pro <= 99 && pro >= 10)
+				_putchar((pro / 10) + 48);
+				_putchar((pro % 10) + 48);
+		}
+		_putchar('\n');
+		}
+	}
+}
