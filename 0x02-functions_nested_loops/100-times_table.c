93be056c4a8bbccee799351141613b330d8f6b39
mohamed <msam13716@gmail.com>
2023-09-12 14:02:20 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ void print_times_table(int n)
 				_putchar((pro / 100) + 48);
 				_putchar((pro / 10) % 10 + 48);
 			}
-			else if (pro <= 99 && pro >= 10)
+			 if (pro <= 99 && pro >= 10)
 				_putchar((pro / 10) + 48);
 				_putchar((pro % 10) + 48);
 		}
