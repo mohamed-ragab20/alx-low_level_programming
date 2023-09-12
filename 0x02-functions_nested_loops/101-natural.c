e2e9c0c10e643c96553d5fddc377359b7d8690b8
mohamed <msam13716@gmail.com>
2023-09-12 14:15:05 -0700

    My commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Discription: computes the sum of all
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+	if ((num % 3 ==  0) || (num % 5 == 0))
+		sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
