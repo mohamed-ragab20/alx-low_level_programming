7da683e3bc48a8a2eea646290cb138dfadfc524c
mohamed <msam13716@gmail.com>
2023-09-12 19:50:35 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,12 +12,13 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
-			for (mult = 1; mult <= n; ++mult)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				prod = num * mult;
 				if (prod <= 9)
