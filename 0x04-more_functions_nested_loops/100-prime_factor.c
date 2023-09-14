8145602699c282f5f680e7144ed1ba1fa0d1f4fa
mohamed <msam13716@gmail.com>
2023-09-14 11:29:51 -0700

    My commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -23,4 +23,40 @@ double _sqrt(double i)
 	return (sqrt);
 }
 
+/**
+ * largest_prime_factor - find and prints the largest
+ *
+ * @num: number to find
+*/
 
+void largest_prime_factor(long int num)
+{
+	int pr, la;
+
+	while (num % 2 == 0)
+		num = num / 2;
+	for (pr = 3; pr <= _sqrt(num); pr += 2)
+	{
+		while (num % pr == 0)
+		{
+			num = num / pr;
+			la = pr;
+		}
+	}
+	if (num > 2)
+		la = num;
+	printf("%d\n", la);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	largest_prime_factor(612852475143);
+
+	return (0);
+}
