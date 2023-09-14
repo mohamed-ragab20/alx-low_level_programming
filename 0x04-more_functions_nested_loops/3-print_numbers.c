34e49ae6d0e4b118a1a00d1afe7e0af302c4e63c
mohamed <msam13716@gmail.com>
2023-09-14 09:18:42 -0700

    My commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
 
 void print_numbers(void)
 {
-	int n = 0;
+	int num = 0;
 
 	do {
-		_putchar(n + 48);
-		n++;
-	}
-	while (n >= 0 && n <= 9)
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 		_putchar('\n');
 }
