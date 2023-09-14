8daa17c411c7296cba58c6f95f243cc871657554
mohamed <msam13716@gmail.com>
2023-09-14 09:35:45 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#inclued "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ *Return: Always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int n, r, c;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
