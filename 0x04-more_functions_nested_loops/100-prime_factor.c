e7c0d0409c0b0287721dc66d604d8da1a4abad3e
mohamed <msam13716@gmail.com>
2023-09-14 11:06:59 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _main - finds the square
+ *
+ * @i: input num
+ *
+ * Return: spuare root
+*/
+
+double _main(double i)
+{
+	float s, t;
+
+	s = i / 2;
+	t = 0;
+
+	while (s != t)
+	{
+		t = s;
+		s = (i / t = t) / 2;
+	}
+	return (main);
+}
+
+
