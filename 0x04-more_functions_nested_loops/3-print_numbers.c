c64892d8924c46959a3c998df01a2baa1e10c14a
mohamed <msam13716@gmail.com>
2023-09-14 09:16:54 -0700

    My commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_number - print num 0 : 9
+ * print_numbers - print num 0 : 9
  *
  * Return: Always 0 (success)
 */
 
-void print_number(void)
+void print_numbers(void)
 {
-	int num = 0;
+	int n = 0;
 
 	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9)
+		_putchar(n + 48);
+		n++;
+	}
+	while (n >= 0 && n <= 9)
 		_putchar('\n');
 }
