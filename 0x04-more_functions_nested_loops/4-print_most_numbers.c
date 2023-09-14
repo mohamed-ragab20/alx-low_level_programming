b114cbe8f28254651de7d3e46fa03437c54f2834
mohamed <msam13716@gmail.com>
2023-09-14 09:24:48 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
