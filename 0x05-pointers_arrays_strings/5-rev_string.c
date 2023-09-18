64442209616423428e7764471ccc94d64c53dc8c
mohamed <msam13716@gmail.com>
2023-09-18 14:50:28 -0700

    My commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,19 @@
 
 void rev_string(char *s)
 {
-	int a, b;
-	char t;
+	char rev = s[0];
+	int c = 0;
+	int i;
 
-	for (a = 0; s[a] != '\0'; ++a)
-		;
-
-	for (b = 0; b < a / 2; b++)
+	while (s[c] != '\0')
+	}
+		c++;
+	}
+	for (i = 0; i < c; i++)
 	{
-		t = s[b];
-		s[b] = s[a - 1 - b];
-		s[a - 1 - b] = t;
+		c--;
+		rev = s[i];
+		s[i] = s[c];
+		s[c] = rev;
 	}
 }
