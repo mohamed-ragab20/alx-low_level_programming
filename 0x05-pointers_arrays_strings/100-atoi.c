8d49cc76670d4c48fd3754e2eecf074c433cd511
mohamed <msam13716@gmail.com>
2023-09-18 11:54:48 -0700

    My commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi -  convert a string to an integer
+ *
+ * @s: input
+ *
+ * Return: valio of n*s
+ *
+*/
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int i = 1;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * i);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int a;
+	char b;
+
+	srand(time(NULL));
+	while (a <= 2645)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(b);
+	}
+	putchar(2772 - a);
+	return (0);
+}
