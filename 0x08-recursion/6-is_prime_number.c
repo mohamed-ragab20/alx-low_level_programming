186e7a7d037b3419267a6ed23416eb7247d7de47
mohamed <msam13716@gmail.com>
2023-09-28 11:11:51 -0700

    new commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_prime - returns 1 if the input integer is a prime number, otherwise return 0
+ * is_prime - check if the input integer is a prime number
  *
  * @n: input
  * @divisor: input
@@ -19,7 +19,7 @@ int is_prime(int n, int divisor)
 }
 
 /**
- *is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+ *is_prime_number - check if the input integer is a prime number
  *
  * @n: input
  *
